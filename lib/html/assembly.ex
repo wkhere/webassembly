4fb9c18a566e848cec7da90e91a8d68e88864e87
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-29 22:02:41 +0200

    assembly state: stricter types

--- lib/html/assembly.ex
+++ lib/html/assembly.ex
@@ -3,18 +3,20 @@ alias Rockside.HTML
 defmodule HTML.Assembly do
 
   defmodule St do
+    require HTML.Assembly.Types, as: T
+
     defstruct stack: []
-    @type t :: %St{stack: list}
+    @type t :: %St{stack: [T.content]}
 
     @spec new() :: t
     def new, do: %__MODULE__{}
 
-    @spec push(t, any) :: t
+    @spec push(t, T.content) :: t
     def push(st = %{stack: s}, elem) do
       %{st | stack: [elem|s]}
     end
 
-    @spec release(t) :: list
+    @spec release(t) :: [T.content]
     def release(%{stack: s}), do: Enum.reverse(s)
   end
 
