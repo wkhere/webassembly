1d0bbfe30485e2e4bf130b3f0c4dbdbf74939a2e
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-29 22:02:28 +0200

    assembly state: basic types

--- lib/html/assembly.ex
+++ lib/html/assembly.ex
@@ -4,13 +4,17 @@ defmodule HTML.Assembly do
 
   defmodule St do
     defstruct stack: []
+    @type t :: %St{stack: list}
 
+    @spec new() :: t
     def new, do: %__MODULE__{}
 
+    @spec push(t, any) :: t
     def push(st = %{stack: s}, elem) do
       %{st | stack: [elem|s]}
     end
 
+    @spec release(t) :: list
     def release(%{stack: s}), do: Enum.reverse(s)
   end
 
