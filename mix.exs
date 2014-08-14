9cb0d1b68afc045c0712ff91f4a94039fa40faab
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 01:40:20 +0200

    cleanup mix stuff

--- mix.exs
+++ mix.exs
@@ -8,23 +8,11 @@ defmodule Webassembly.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
+ defp deps do
     []
   end
 end
