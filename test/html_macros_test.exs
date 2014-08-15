9a910932541cc479f7ab99b8f38f4ad47d64afd8
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 14:58:01 +0200

    meta test all html macros - now 100% coverage! :D

---
+++ test/html_macros_test.exs
@@ -0,0 +1,29 @@
+defmodule WebAssembly.HTMLMacrosTest do
+  use    ExUnit.Case
+  #import WebAssembly.TestHelper
+  use    WebAssembly
+
+  WebAssembly.DSL.html_void_tags
+  |> Enum.each fn tag ->
+    test "#{tag} test" do
+      doc = builder do: unquote(tag)(class: "foo")
+      doc_no_attrs = builder do: unquote(tag)()
+      tag = unquote(tag)
+      assert doc == [["<#{tag} ", [~s/class="foo"/], " />"]]
+      assert doc_no_attrs == ["<#{tag} />"]
+    end
+  end
+
+  WebAssembly.DSL.html_nonvoid_tags
+  |> Enum.each fn tag ->
+    test "#{tag} test" do
+      doc = builder do: unquote(tag)([class: "foo"], "content")
+      doc_no_attrs = builder do: unquote(tag)("content")
+      tag = unquote(tag)
+      assert doc ==
+        [["<#{tag} ", [~s/class="foo"/], ">"], "content", "</#{tag}>"]
+      assert doc_no_attrs ==
+        ["<#{tag}>", "content", "</#{tag}>"]
+    end
+  end 
+end
