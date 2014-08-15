47a3fe49b7f36c202b2b2dd45b60930095096444
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 19:34:17 +0200

    basic Core.St docs

--- lib/core.ex
+++ lib/core.ex
@@ -3,19 +3,31 @@ alias WebAssembly.Core
 defmodule Core do
 
   defmodule St do
+    @moduledoc """
+    State of markup assembly in the current scope.
+    """
     require WebAssembly.Types, as: T
 
     defstruct stack: []
     @type t :: %St{stack: [T.content]}
 
+    @doc """
+    Creates new empty state.
+    """
     @spec new() :: t
     def new, do: %__MODULE__{}
 
+    @doc """
+    Pushes `value` into the `state`.
+    """
     @spec push(t, T.content) :: t
-    def push(%{stack: s} = state, elem) do
-      %{state | stack: [elem|s]}
+    def push(%{stack: s} = state, value) do
+      %{state | stack: [value|s]}
     end
 
+    @doc """
+    Releases the `state`, returning values in the order of pushing.
+    """
     @spec release(t) :: [T.content]
     def release(%{stack: s} = _state), do: Enum.reverse(s)
   end
