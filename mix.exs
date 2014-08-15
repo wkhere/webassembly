f2da3ca560c1fc9e62b59e7411349b7957173ba7
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 03:46:46 +0200

    bump ver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Webassembly.Mixfile do
 
   def project do
     [app: :webassembly,
-     version: "0.0.1-dev",
+     version: "0.1.0",
      elixir: "~> 0.15.0",
      deps: deps,
      description: description,
