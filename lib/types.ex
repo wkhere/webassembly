ca26db68259d9c18dc94c5ea3bdff594ad23d3a6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 13:02:05 +0200

    cleanup types a bit
    * relax content type to allow numbers
    * rename type for void tags

--- lib/types.ex
+++ lib/types.ex
@@ -2,8 +2,8 @@ defmodule WebAssembly.Types do
 
   @type tagname  :: atom
   @type attrs    :: [{atom, String.t}]
-  @type content  :: String.t | out_tag | [String.t | out_tag]
-  @type out_tag1 :: [String.t | [String.t]]
+  @type content  :: number | String.t | out_tag | [String.t | out_tag]
   @type out_tag  :: [String.t | [String.t] | content]
+  @type out_tag_void :: [String.t | [String.t]]
 
 end
