1fb2458b5c24327958f4894a654411ac56c6f772
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 03:41:27 +0200

    hexify

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Webassembly.Mixfile do
      version: "0.0.1-dev",
      elixir: "~> 0.15.0",
      deps: deps,
+     description: description,
+     package: package,
      test_coverage: [tool: ExCoveralls]]
   end
 
@@ -14,7 +16,23 @@ defmodule Webassembly.Mixfile do
       description: 'Web DSL']
   end
 
- defp deps do
+  defp deps do
     [{:excoveralls, "~> 0.3.2", only: :test}]
+  end
+
+  defp description do
+    """
+    WebAssembly is a web DSL for Elixir.
+
+    You create html structure straight using Elixir blocks.
+    """
+  end
+
+  defp package do
+    [ contributors: ["Wojciech Kaczmarek"],
+      licenses: ["BSD"],
+      description: description,
+      links: %{"GitHub" => "https://github.com/herenowcoder/webassembly"}
+    ]
   end
 end
