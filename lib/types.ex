b6c3e444d94a56670813e0fce4c5b8997d856c6c
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-15 14:03:38 +0200

    again relax content typespec, now allowing atom

--- lib/types.ex
+++ lib/types.ex
@@ -2,7 +2,7 @@ defmodule WebAssembly.Types do
 
   @type tagname  :: atom
   @type attrs    :: [{atom, String.t}]
-  @type content  :: number | String.t | out_tag | [String.t | out_tag]
+  @type content  :: atom | number | String.t | out_tag | [String.t | out_tag]
   @type out_tag  :: [String.t | [String.t] | content]
   @type out_tag_void :: [String.t | [String.t]]
 
