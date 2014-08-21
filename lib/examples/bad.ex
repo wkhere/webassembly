d761b41dfec8b6af569c44e0f0070aac9aed5583
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-21 02:21:25 +0200

    bad example for dialyzer :]

---
+++ lib/examples/bad.ex
@@ -0,0 +1,16 @@
+defmodule WebAssembly.BadExamples do
+  # these should make dialyzer crying
+
+  @moduledoc false
+
+  #alias WebAssembly.Types, as: T
+
+  use WebAssembly
+
+  def bad1 do
+    builder do
+      span 1
+      span :not_text
+    end
+  end
+end
