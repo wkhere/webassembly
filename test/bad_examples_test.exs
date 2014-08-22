2b01f59eedc2a6fd02ceefbe03469d59e03e06e8
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-22 11:24:26 +0200

    assert that bad example also raises
    ..not only that dialyzer complains about it

---
+++ test/bad_examples_test.exs
@@ -0,0 +1,10 @@
+defmodule WebAssembly.BadExamplesTest do
+  use ExUnit.Case
+  import WebAssembly.TestHelper
+  import WebAssembly.BadExamples
+
+  test "bad examples" do
+    assert_raise ArgumentError, fn -> bad1 |> flush end
+  end
+
+end
