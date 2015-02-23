821196921f27424a22839a7d55d6e39657f1d558
Wojciech Kaczmarek <wojtekk@kofeina.net>
2015-02-23 03:47:26 +0100

    dev ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebAssembly.Mixfile do
   def project do
     [app: :webassembly,
      docs: [main: WebAssembly],
-     version: "0.3.4",
+     version: "0.4.0-dev",
      elixir: "~> 1.0.0",
      deps: deps,
      description: description,
