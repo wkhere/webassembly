5955eff0bff8d5c9eee3185c9674c8b033bfbfd4
Wojciech Kaczmarek <wojciech.kaczmarek@pearson.com>
2015-02-24 10:55:36 +0100

    dev ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebAssembly.Mixfile do
   def project do
     [app: :webassembly,
      docs: [main: WebAssembly],
-     version: "0.4.0",
+     version: "0.4.1-dev",
      elixir: "~> 1.0.0",
      deps: deps,
      description: description,
